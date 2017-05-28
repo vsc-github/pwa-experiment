8e8b948fb7c18326c3d0ace6c693067d439cd4b7
Vishwas <batty.wayn3@gmail.com>
2017-05-28 15:33:37 +0530

    Added basic app styles

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-header">
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="menu">
+              <div className="menu-item">One</div>
+              <div className="menu-item">Two</div>
+              <div className="menu-item">Three</div>
+              <div className="menu-item">Four</div>
+          </div>
+          <div className="feed">
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+              <div className="feed-item"></div>
+          </div>
       </div>
     );
   }
